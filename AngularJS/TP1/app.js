50d556431a4ecfc9e824ea9276413a2432aeb130
AlissaCosson <alissa.cosson@gmail.com>
2017-01-30 10:06:26 +0100

    Bouton Effacer fonctionnel
    On peut maintenant effacer le contenu du textarea grâce au bouton prévu
    à cet effet

--- AngularJS/TP1/app.js
+++ AngularJS/TP1/app.js
@@ -3,3 +3,4 @@
  */
 
 angular.module("TP1", []);
+
--- AngularJS/TP1/noteController.js
+++ AngularJS/TP1/noteController.js
@@ -2,15 +2,10 @@
  * Created by Pc maison on 27/01/2017.
  */
 
-angular.module("TP1").controller("noteController", function(){
-    var self=this;
-    var message="Hello";
-    this.msg="Coucou";
-    this.getMsg=function(){
-        return self.msg;
+angular.module("TP1").controller("noteController", ['$scope', function($scope){
+    $scope.txt='';
+
+    $scope.reset=function(){
+        $scope.txt='';
     };
-
-    this.enregistrer=function(){
-
-    }
-});
+}]);
