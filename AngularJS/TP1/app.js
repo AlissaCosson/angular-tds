07a111e7c44002ab314d396d401bf8c6ed03dbe1
AlissaCosson <alissa.cosson@gmail.com>
2017-02-02 13:33:50 +0100

    Ajout des cookies et organisation du code
    L'application est terminée !

--- AngularJS/TP1/app.js
+++ AngularJS/TP1/app.js
@@ -2,5 +2,5 @@
  * Created by Pc maison on 27/01/2017.
  */
 
-angular.module("TP1", []);
+angular.module("TP1", ["ngCookies"]);
 
--- AngularJS/TP1/noteController.js
+++ AngularJS/TP1/noteController.js
@@ -2,10 +2,47 @@
  * Created by Pc maison on 27/01/2017.
  */
 
-angular.module("TP1").controller("noteController", ['$scope', function($scope){
-    $scope.txt='';
 
-    $scope.reset=function(){
-        $scope.txt='';
+var app = angular.module('TP1',["ngCookies"]);
+app.controller('noteController', ["$cookies",function(cookies) {
+
+
+
+    var info = "";
+    this.value = cookies.get('messageNote');
+    var self=this;
+
+    this.count = function() {
+
+        if(self.value != "" && self.value!=cookies.get('messageNote') )
+
+            info = "Message modifié";
+
+        return 100-self.value.length;
     };
+
+    this.clear = function(){
+
+        info = "";
+
+        self.value="";
+    };
+
+    this.save = function(){
+
+        info="Message sauvegardé";
+
+        if (self.value != "") {
+
+            cookies.put('messageNote', self.value);
+
+        }
+    };
+
+
+    this.show = function(){
+
+        return info;
+    };
+
 }]);
