6ecd8ecff63fd949c7701dfa6714c2f68057a8fe
AlissaCosson <alissa.cosson@gmail.com>
2017-02-17 09:58:30 +0100

    Mise en place du code promo
    On peut désormais écrire un code promo. Cependant les codes ne
    retournent rien pour le moment.

--- AngularJS/TP2/serviceController.js
+++ AngularJS/TP2/serviceController.js
@@ -5,6 +5,9 @@
 var app=angular.module("TP2");
 app.controller("serviceController", [ function(){
 
+    var code="";
+    var self=this.value;
+
     this.services= [
         {
             "name": "Web Development",
@@ -22,7 +25,21 @@ app.controller("serviceController", [ function(){
         "name": "Formation",
         "price": 220,
         "active":false
-    }]
+    }];
+
+    this.promos=[
+        {
+            "code": "B22",
+            "remise": 0.05
+        },
+        {
+            "code": "AZ",
+            "remise": 0.01
+        },
+        {
+        "code": "UBOAT",
+        "remise" :0.02
+    }];
 
     this.price=300;
     this.activer=function(service){
@@ -35,18 +52,45 @@ app.controller("serviceController", [ function(){
             this.value=this.value-1;
             this.price=this.price-service.price;
         }
-    }
+    };
 
     this.value=1;
     this.count=function(service){
         return this.value;
-    }
+    };
 
     this.total=function(price){
         return this.price;
+    };
+
+    this.visible=false;
+    this.setVisible=function(){
+        this.visible=!this.visible;
+    };
+
+    this.estVisible=function(promo){
+        return this.visible;
+    };
+
+    this.remise=0;
+
+
+    this.remisePromo=function(code){
+        if(this.code=="B22") {
+            this.remise = this.price * 0.05;
+        }
+        else if(this.code=="AZ") {
+            this.remise = this.price * 0.01;
+        }
+        else if(this.code=="UBOAT") {
+            this.remise = this.price * 0.02;
+        }
+        else {
+            this.remise = 0;
+        }
+
+        return this.remise;
     }
-
-
 
 
 }]);
