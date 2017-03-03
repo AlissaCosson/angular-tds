a2a4bd9e41a89f2d257b90096dfdc0c3f4b55121
AlissaCosson <alissa.cosson@gmail.com>
2017-03-03 11:01:32 +0100

    TP5 - Listes déroulantes, checkbox Historique et swap
    Les listes déroulantes contiennent les bonnes données, le swap est
    fonctionnel et la checkbox de l'historique permet de montrer
    l'historique ou de le masquer

--- AngularJS/TP5/currencyController.js
+++ AngularJS/TP5/currencyController.js
@@ -5,7 +5,9 @@
 var app=angular.module("TP5");
 app.controller("currencyController", ['$http',  function($http){
 
-    var self=this.value;
+    var self=this;
+
+    this.currencies;
 
     this.visibleHistorique=true;
     this.setVisibleHistorique=function(){
@@ -16,24 +18,38 @@ app.controller("currencyController", ['$http',  function($http){
         return this.visibleHistorique;
 
     };
-    
+
 
     //L'initialisation de la variable currencies grâce au fichier JSON se fera dans le contrôleur par une requête Ajax :
 
+    this.convertir=function() {
 
-   /* $http.jsonp('https://free.currencyconverterapi.com/api/v3/convert?compact=y&q='+from.code+'_'+to.code, {jsonpCallbackParam: 'callback'})
-        .then(function(response) {
-            self.result=response.data[self.from.code+'_'+self.to.code].val;
+        $http.jsonp('https://free.currencyconverterapi.com/api/v3/convert?compact=y&q=' + from.code + '_' + to.code, {jsonpCallbackParam: 'callback'})
+            .then(function (response) {
+                self.result = response.data[self.from.code + '_' + self.to.code].val;
 
-        });
+            })
+    };
 
     $http.get('app/data/currencymap.json').
     then(function(response) {
             self.currencies = response.data;
+            self.from=self.currencies['EUR'];
+            self.to=self.currencies['USD'];
         },
+
         function(response) {
             console.log("Erreur avec le statut Http : "+response.status);
-        });*/
+        });
+
+        this.swap=function(){
+            change=self.from;
+            self.from= self.to;
+            self.to=change;
+        }
+
+
+
 
 
 
